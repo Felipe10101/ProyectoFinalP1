6995a28608c7c1f78586aa33f8dfd1b4c2f90d8c
ThomasDC2 <jesust.diazc@uqvirtual.edu.co>
2025-10-28 10:23:04 -0500

    Add; LinkedList pertenecientes a  Academia

--- ProjectMusica/ProjectMusica/src/main/java/co/edu/uniquindio/poo/projectmusica/model/Academia.java
+++ ProjectMusica/ProjectMusica/src/main/java/co/edu/uniquindio/poo/projectmusica/model/Academia.java
@@ -1,11 +1,20 @@
 package co.edu.uniquindio.poo.projectmusica.model;
+import java.util.LinkedList;
 
 public class Academia {
     private String nombre, id;
+    private LinkedList<Curso> ListCurso;
+    private LinkedList<Estudiante> ListEstudiante;
+    private LinkedList<Profesor> ListProfesor;
+    private LinkedList<AdministradorAcademico> ListAministradorAcademico;
 
     public Academia(String nombre, String id) {
         this.nombre = nombre;
         this.id = id;
+        this.ListCurso = new LinkedList<>();
+        this.ListEstudiante = new LinkedList<>();
+        this.ListProfesor = new LinkedList<>();
+        this.ListAministradorAcademico = new LinkedList<>();
     }
 
     public String getNombre() {
