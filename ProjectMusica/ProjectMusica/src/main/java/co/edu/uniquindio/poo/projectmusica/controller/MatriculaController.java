fe723cd7da1753659100c9e46b19d8e3237ca7ef
ThomasDC2 <jesust.diazc@uqvirtual.edu.co>
2025-10-28 08:59:28 -0500

    Add; Clases Controlller

---
+++ ProjectMusica/ProjectMusica/src/main/java/co/edu/uniquindio/poo/projectmusica/controller/MatriculaController.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.poo.projectmusica.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MatriculaController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ ProjectMusica/ProjectMusica/src/main/java/co/edu/uniquindio/poo/projectmusica/controller/ReporteProgresoController.java
@@ -0,0 +1,14 @@
+AcademiaController.javapackage co.edu.uniquindio.poo.projectmusica.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ReporteProgresoController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
